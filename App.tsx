50e040e35446ce29127dea6ac9760397d91154d6
Iano Maciel <ianomaciel6385@gmail.com>
2021-09-05 23:27:55 -0400

    Main file for test execution

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/Login';
 import { RecorverPass } from './src/RecorverPass';
+import { RegistrationScreen } from './src/RegistrationScreen';
 
 export default function App() {
   return (
-    <RecorverPass/>
     //<Login/>
+    //<RecorverPass/>
+    <RegistrationScreen/>
   );
 }
 
