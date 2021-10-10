bb2b6b909c649363086bf03b0f34b24f858252a6
GeiseSaunier <geisesaunier3939@gmail.com>
2021-10-09 22:01:51 -0700

    feat(#15): adding react-navegation
    Co-authored-by: IanoMaciel <ianomaciel6385@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ExpertScreen } from './src/view/ExpertScreen';
 import Login from './src/view/Login';
 import { ProfileScreen } from './src/view/ProfileScreen';
 import { RecorverPass } from './src/view/RecorverPass';
@@ -7,9 +8,11 @@ import { RegistrationScreen } from './src/view/RegistrationScreen';
 export default function App() {
   return (
     //<Login/>
-    <RecorverPass/>
+    //<RecorverPass/>
     //<RegistrationScreen/>
     //<ProfileScreen/>
+    <ExpertScreen/>
+
   );
 }
 
